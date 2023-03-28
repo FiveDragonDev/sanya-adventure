f4dc42267f8e6534e156b204609551484b2a994d
FiveDragonYT <104275531+FiveDragonYT@users.noreply.github.com>
2023-03-28 23:42:08 +0300

    V 0.0.6 Shop and Social Update

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from time import sleep
 
 defult_actions: str = ['Порыться в мусоре — 1', 'Сдать бутылки — 2', 'Посмотреть содержимое инвентаря — 3',
 					   'Посмотреть информацию — 4', 'Съесть что-нибудь — 5', 'Вылечить раны чем-то — 6',
-					   'Остановить игру — 0']
+					   'Сходить в магаз — 7', 'Остановить игру — 0']
 passport_actions: str = ['Сменить имя — 1', 'Продолжить игру — 0']
 items = {
 	'food': {
-		'trash': ['Кусок хлеба', 'Заплесневелый кусок хлеба', 'Яблочный огрызок']
+		'trash': ['Кусок хлеба', 'Заплесневелый кусок хлеба', 'Яблочный огрызок'],
+		'defult': ['Яблоко;2', 'Доширак;15', 'Ролтон;5']
 	},
 	'clothes': {
-		'trash': ['Рваная рубашка', 'Грязные джинсы']
+		'trash': ['Рваная рубашка', 'Грязные джинсы'],
+		'defult': ['Рубашка;7']
 	},
 	'consumables': {
-		'trash': ['Бутылка', 'Грязный бинт'],
-		'defult': ['Стерильный бинт']
+		'trash': ['Бутылка', 'Грязный бинт', 'Кусок белого халата'],
+		'defult': ['Стерильный бинт;3']
 	}
 }
 item_categories = ['food', 'clothes', 'consumables']
@@ -37,6 +39,12 @@ debug = False
 health_status = (health_statuses[1] if wounded
 				 else health_statuses[3]) if not hungry \
 	else (health_statuses[2] if wounded else health_statuses[0])
+social_statuses = ['Бомж', 'Главный бомж', 'Кассир', 'Менеджер', 'Главный менеджер', 'Начальник', 'Босс',
+				   'Главный босс',
+				   'Бизнесмен']
+social_status = social_statuses[0]
+social_rating = 0
+null_social_rating = 0
 
 print(f'Добро пожаловать в приключения бомжа Сани!')
 print(f'Ваша цель одна: ВЫЖИТЬ.')
@@ -49,12 +57,12 @@ print(f'https://github.com/FiveDragonYT/sanya-adventure\n\n')
 defult_actions_list: str = '; '.join(defult_actions)
 print(f'Доступные действия: {defult_actions_list}.')
 while True:
-	action = input('Ваше действие: ')
+	action = input('Ваше действие: ').lower().strip()
 
 	if not action:
 		print(f'Доступные действия: {defult_actions_list}.')
 	elif action.startswith('/'):
-		action = action.replace('/', '', 1).lower().strip()
+		action = action.replace('/', '', 1)
 
 		if 'debug' == action:
 			if debug:
@@ -63,26 +71,41 @@ while True:
 				continue
 			debug = True
 			print('Режим ДЕБАГ включён!')
-		elif 'heal' == action:
-			health = 100
-			wounded = False
-			hungry = False
-			hungry_time = -15
-			if monies <= 0:
-				monies = 1000
-			print(f'Вы полностью здоровы!')
-		elif 'monies;' in action:
-			try:
+		elif debug:
+			if 'heal' == action:
+				health = 100
+				wounded = False
+				hungry = False
+				hungry_time = -15
+				if monies <= 0:
+					monies = 1000
+				print(f'Вы полностью здоровы!')
+			elif 'monies;' in action:
+				try:
+					h = action.split(';')
+					monies = int(h[1].strip().lower())
+					print(f'Новое кол-во денег: {monies}')
+				except:
+					pass
+			elif 'social;' in action:
 				h = action.split(';')
-				monies = int(h[1].strip().lower())
-				print(f'Новое кол-во денег: {monies}')
-			except:
-				pass
-		elif 'suicide' == action:
-			health = 0
-			if debug:
-				print('Мёртв...')
-				break
+				null_social_rating = int(h[1].strip().lower())
+				social_rating = null_social_rating
+				for i in range(round(social_rating / 100)):
+					if null_social_rating >= 100:
+						null_social_rating -= 100
+						i: int = social_statuses.index(social_status)
+						i += 1
+						social_status = social_statuses[i]
+				print(f'Новый соц. статус: {social_status}\n'
+					  f'Новое кол-во соц. рейтинга: {social_rating}')
+			elif 'suicide' == action:
+				health = 0
+				if debug:
+					print('Мёртв...')
+					break
+			else:
+				print('Команда не существует.')
 		else:
 			print('Команда не существует.')
 	else:
@@ -118,6 +141,21 @@ while True:
 			print(f'Вы получили {give_monies} рублей продав {len(bottles)} бутылок')
 			monies += give_monies
 			items_in_inventory['consumables'] = list(set(items_in_inventory['consumables']) - set(bottles))
+			if len(bottles) >= 2:
+				if randint(0, 100) <= 50:
+					getting_social_rating = randint(5, 20)
+					print(f'Люди подумали, что вы волонтёр и убираетесь на улице. '
+						  f'Социальный рейтинг +{getting_social_rating}')
+					null_social_rating += getting_social_rating
+					social_rating += getting_social_rating
+					for i in range(round(social_rating / 100)):
+						if null_social_rating >= 100:
+							null_social_rating -= 100
+							i: int = social_statuses.index(social_status)
+							i += 1
+							social_status = social_statuses[i]
+					print(f'Ваш социальный статус: {social_status}\n'
+						  f'Ваше кол-во социального рейтинга: {social_rating}')
 			bottles.clear()
 		elif '3' == action:
 			items_in_inventory.update()
@@ -136,7 +174,8 @@ while True:
 			print('')
 			print(f'Имя: {player_name}\n'
 				  f'{monies} рублёв в кармане\n'
-				  f'{health_status}, здоровье: {health if not wounded and not hungry else health - 1}%\n')
+				  f'{health_status}, здоровье: {health if not wounded and not hungry else health - 1}%\n'
+				  f'Ваш социальный статус: {social_status}\n')
 			passport_actions_list: str = '; '.join(passport_actions)
 			print(f'Доступные действия: {passport_actions_list}.')
 			while True:
@@ -175,14 +214,21 @@ while True:
 				hungry_time = 0
 		elif '6' == action:
 			if not debug:
-				if not wounded:
+				if not wounded and health > 90:
 					print('Вы не ранены и решаете не тратить расходники в пустую.')
 					continue
 				elif len(items_in_inventory['consumables']) <= 0:
 					print('У вас нет расходников.')
 					continue
 				consumables = items_in_inventory['consumables']
-				consumables[:] = (value for value in consumables if value != 'Бутылка')
+				try:
+					consumables[:] = (value for value in consumables if value != 'Бутылка')
+				except:
+					print('Произошла некоторая ошибка, пожалуйста напишите отзыв и приложите к нему скриншот ошибки')
+					continue
+				if len(consumables) <= 0:
+					print('У вас нет расходников.')
+					continue
 				consumable = consumables[randint(0, len(consumables) - 1)]
 				wounded = False
 				health += 5
@@ -195,6 +241,58 @@ while True:
 				health += 5
 				if health > 100:
 					health = 100
+		elif '7' == action:
+			item_categories_list = ''
+			for i in range(len(item_categories)):
+				item_categories_list += item_categories[i] + f' — {i + 1}; '
+			print(f'Доступные категории предметов для покупки: {item_categories_list}')
+			category_index = input('Выберите номер категории предметов: ').lower().strip()
+			if len(item_categories) >= int(category_index) >= 1:
+				selected_category = item_categories[int(category_index) - 1]
+				print(f'Выбрана категория: {selected_category}')
+				items_in_category_str = ''
+				items_in_category = items[selected_category]['defult']
+				try:
+					for i in range(len(items[selected_category]['defult'])):
+						items_in_category_str += items[selected_category]['defult'][i] + f' — {i + 1}'
+						if i != len(items[selected_category]['defult'][i]):
+							items_in_category_str += ', '
+				except:
+					print('Произошла некоторая ошибка, пожалуйста напишите отзыв и приложите к нему скриншот ошибки')
+					continue
+				print(print(f'Доступные предметы: {items_in_category_str}'))
+				item_index = input('Введите номер предмета, который хотите купить: ').lower().strip()
+				try:
+					item_index = int(item_index)
+				except TypeError:
+					continue
+				if item_index > len(items_in_category):
+					print('Предмет не найден')
+				else:
+					selected_item = str(items_in_category[item_index - 1])
+					h = selected_item.split(';')
+					item_price = int(h[1])
+					selected_item = h[0]
+					print(f'Вы выбрали {selected_item}, его цена {item_price} рублей')
+					if monies >= item_price:
+						while True:
+							print(f'Купить — 1; Не покупать — 2; ')
+							buy_or_not = input('Ваше действие: ').lower().strip()
+							if '1' == buy_or_not:
+								monies -= item_price
+								items_in_inventory[selected_category].append(selected_item)
+								print(f'Вы успешно купили {selected_item.lower()}! Ваше новое кол-во денег: {monies}')
+								break
+							elif '2' == buy_or_not:
+								print(f'Вы отказались от покупки {selected_item.lower()}')
+								break
+							else:
+								print('Некорректный выбор.')
+					else:
+						print(f'К сожалению у вас не хватает денег, '
+							  f'{selected_item.lower()} стоит {item_price} рублей')
+			else:
+				print('Номера такой категории не существует')
 		elif '0' == action:
 			break
 		else:
@@ -208,15 +306,17 @@ while True:
 			health -= 1
 		# Рандомные события
 		if days_without_events >= 7:
-			event = events[randint(0, len(events) - 1)]
-			print(event)
-			if event == 'На вас напал гопник':
-				health -= 15
-				wounded = True
-			elif event == 'Вы отравились':
-				health -= 5
-			elif event == 'Вы нашли кошелёк на земле':
-				monies += 15
+			if randint(0, 100) <= 75:
+				event = events[randint(0, len(events) - 1)]
+				print(event)
+				if event == 'На вас напал гопник':
+					health -= 15
+					wounded = True
+				elif event == 'Вы отравились':
+					health -= 5
+				elif event == 'Вы нашли кошелёк на земле':
+					monies += 15
+			days_without_events = 0
 
 		# Проверка на здоровье, уберите, чтобы стать бессмертным
 		if not debug:
